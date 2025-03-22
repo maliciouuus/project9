a4b6277ad449e9fcdc66d2b955d3b4386ea93657
VotreNouveauNom <votre.nouvel.email@example.com>
2025-03-22 23:27:35 +0100

    push

---
+++ literevu/literevu/wsgi.py
@@ -0,0 +1,16 @@
+"""
+Configuration WSGI pour le projet literevu.
+
+Il expose le module WSGI comme une variable de niveau supérieur nommée ``application``.
+
+Pour plus d'informations sur ce fichier, voir
+https://docs.djangoproject.com/fr/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "literevu.settings")
+
+application = get_wsgi_application() 
